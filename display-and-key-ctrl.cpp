da12b378320d2a4b7f4b9df012e6b26609033d5c
Emerio Kevin <emeriokevin2320@gmail.com>
2022-05-20 17:54:39 +0700

    Update display-and-key-ctrl.cpp

--- display-and-key-ctrl.cpp
+++ display-and-key-ctrl.cpp
@@ -15,8 +15,9 @@ int hadapTruk = 0, cekHadapTruk = 0;
 float rotPalang = 0.0;
 int cekPalang = 0, PlgRc = 1, PlgGc = 0, PlgBc = 0;
 
-int nLstrk = 10;
-int nGdg = 10;
+int nLstrk = 14;
+int nGdg = 6;
+int loopJalan = 20;
 
 void display() 
 {
@@ -33,12 +34,93 @@ void display()
 	glClearColor(0.53f, 0.8f, 0.92f, 1.0f);
 //	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glDisable(GL_CULL_FACE);
-	// Gambar grid
-	Grid();
+	// Gambar Jalan
 	glPushMatrix();
-		for (int k = 1; k <= 20; k++) {
+		for (int k = 1; k <= loopJalan; k++) {
 			JalanTotal();
-			glTranslatef(-18, 0, 0);
+			glTranslatef(-24, 0, 0);
+		}
+	glPopMatrix();
+	
+	// Trotoar Kiri
+	glPushMatrix();
+		glTranslatef(0, 0, 10);
+		for (int k = 1; k <= loopJalan; k++) {
+			Trotoar(6, 1, 1, 1, 0, 0, 0);
+			glTranslatef(-48, 0, 0);
+		}
+	glPopMatrix();
+	glPushMatrix();
+		glTranslatef(0, 0, 20);
+		for (int k = 1; k <= loopJalan; k++) {
+			Trotoar(6, 0, 0, 0, 1, 1, 1);
+			glTranslatef(-48, 0, 0);
+		}
+	glPopMatrix();
+	glPushMatrix();
+		glRotatef(90, 0, 1, 0);
+		glTranslatef(-14, 0, 20);
+		Trotoar(2, 0, 0, 0, 1, 1, 1);
+	glPopMatrix();
+	glPushMatrix();
+		glRotatef(90, 0, 1, 0);
+		glTranslatef(-14, 0, -458);
+		Trotoar(2, 1, 1, 1, 0, 0, 0);
+	glPopMatrix();
+	glPushMatrix();
+		glColor3f(0.7, 0.7, 0.7);
+		for (int k = 1; k <= loopJalan; k++) {
+			if (k == 1) {
+				Balok(-4, 18, 1, 0, 10, 18);	
+				glTranslatef(-24, 0, 0);
+			} else if (k == loopJalan) {
+				Balok(-2, 22, 1, 0, 10, 18);	
+				glTranslatef(-24, 0, 0);
+			} else {
+				Balok(-4, 22, 1, 0, 10, 18);	
+				glTranslatef(-24, 0, 0);	
+			}
+		}
+	glPopMatrix();
+	
+	// Trotoar Kanan
+	glPushMatrix();
+		glTranslatef(0, 0, -24);
+		for (int k = 1; k <= loopJalan; k++) {
+			Trotoar(6, 0, 0, 0, 1, 1, 1);
+			glTranslatef(-48, 0, 0);
+		}
+	glPopMatrix();
+	glPushMatrix();
+		glTranslatef(0, 0, -34);
+		for (int k = 1; k <= loopJalan; k++) {
+			Trotoar(6, 1, 1, 1, 0, 0, 0);
+			glTranslatef(-48, 0, 0);
+		}
+	glPopMatrix();
+	glPushMatrix();
+		glRotatef(90, 0, 1, 0);
+		glTranslatef(30, 0, 20);
+		Trotoar(2, 0, 0, 0, 1, 1, 1);
+	glPopMatrix();
+	glPushMatrix();
+		glRotatef(90, 0, 1, 0);
+		glTranslatef(30, 0, -458);
+		Trotoar(2, 1, 1, 1, 0, 0, 0);
+	glPopMatrix();
+	glPushMatrix();
+		glColor3f(0.7, 0.7, 0.7);
+		for (int k = 1; k <= loopJalan; k++) {
+			if (k == 1) {
+				Balok(-4, 18, 1, 0, -26, -34);	
+				glTranslatef(-24, 0, 0);
+			} else if (k == loopJalan) {
+				Balok(-2, 22, 1, 0, -26, -34);	
+				glTranslatef(-24, 0, 0);
+			} else {
+				Balok(-4, 22, 1, 0, -26, -34);	
+				glTranslatef(-24, 0, 0);	
+			}
 		}
 	glPopMatrix();
 	
@@ -46,15 +128,23 @@ void display()
 	//gambar aseli cuy 
 	glPushMatrix();
 		glPushMatrix();
-			glTranslatef(0.0, 2.3, 2);
+			glTranslatef(-50.5, 2.3, 5);
 			glRotatef(rotPalang, 1, 0, 0);
 			glTranslatef(0.0, -2, -2);
-			Palang(4, 1, 0, 0, 1, 1, 1);	
+			Palang(7, 1, 0, 0, 1, 1, 1);	
 		glPopMatrix();
 		glPushMatrix();
+			glTranslatef(-50.5, 0, 3);
 			PelengkapPalang(0.6, 0.6, 0.6, PlgRc, PlgGc, PlgBc);
 			glTranslatef(-0.999, 0, 0);
 			PelengkapPalang(0.6, 0.6, 0.6, PlgRc, PlgGc, PlgBc);
+		glPopMatrix();
+		// halang rintang
+		glPushMatrix();
+			glColor3f(0.64, 0.46, 0.31);
+			Balok(-150, -155, 5, 0, 3, -3);
+			Balok(-190, -195, 5, 0, -13, -19);
+			Balok(-230, -235, 5, 0, -5, -11);
 		glPopMatrix();
 		glPushMatrix();
 			glTranslatef(posTrukX, 0, posTrukZ);
@@ -65,23 +155,67 @@ void display()
 				0.1, 0.1, 0.1
 			);
 		glPopMatrix();
+		// tiang kiri
 		glPushMatrix();
-			glTranslatef(-20, 0, 6);
+			glTranslatef(14.75, 0, 6);
 			glRotatef(90, 0, 1, 0);
 			for (int i = 1; i <= nLstrk; i++) {
-				TiangListrik(0.6, 0.6, 0.6, 0.2, 0.2, 0.2);
+				TiangListrik(0.6, 0.6, 0.6, 0.2, 0.2, 0.2, 1, 1, 0);
 				if (i < nLstrk) {
-					KabelListrik(0.1, 0.1, 0.1, -30);
+					KabelListrik(0.1, 0.1, 0.1, -36);
 				}
-				glTranslatef(0, 0, -30);	
+				glTranslatef(0, 0, -36);	
 			}
 		glPopMatrix();
+		// tiang kanan
 		glPushMatrix();
-			glTranslatef(0, 0, 30);
-			for (int k = 1; k <= nGdg; k++) {
-				Gedung(0, 0.1, 1.0, 0, 1, 0, 1, 0, 0);
-				glTranslatef(-90, 0, 0);
+			glRotatef(270, 0, 1, 0);
+			glTranslatef(-22, 0, 452.75);
+//			glRotatef(90, 0, 1, 0);
+			for (int i = 1; i <= nLstrk; i++) {
+				TiangListrik(0.6, 0.6, 0.6, 0.2, 0.2, 0.2, 1, 1, 0);
+				if (i < nLstrk) {
+					KabelListrik(0.1, 0.1, 0.1, -36);
+				}
+				glTranslatef(0, 0, -36);	
 			}
+		glPopMatrix();
+		// gedung kiri
+		glPushMatrix();
+			glTranslatef(16, 0, 20.001);
+			for (int k = 1; k <= nGdg; k++) {
+				if (k == nGdg) {
+					Gedung2(0, 0.1, 1.0, 0, 1, 0);
+					break;
+				}
+				Gedung3(0, 0.1, 1.0, 0, 1, 0, 1, 0, 0);
+				glTranslatef(-85.1, 0, 0);
+			}
+		glPopMatrix();
+		// gedung kanan
+		glPushMatrix();
+			glRotatef(180, 0, 1, 0);
+			glTranslatef(460, 0, 36.001);
+			for (int k = 1; k <= nGdg; k++) {
+				if (k == nGdg) {
+					Gedung2(0, 0.1, 1.0, 0, 1, 0);
+					break;
+				}
+				Gedung3(0, 0.1, 1.0, 0, 1, 0, 1, 0, 0);
+				glTranslatef(-85.1, 0, 0);
+			}
+		glPopMatrix();
+		// gedung blkg truk
+		glPushMatrix();
+			glRotatef(90, 0, 1, 0);
+			glTranslatef(50, 0, 20.001);
+			Gedung3(1, 1, 1, 0, 1, 1, 1, 1, 0);
+		glPopMatrix();
+		// gedung ujung
+		glPushMatrix();
+			glRotatef(270, 0, 1, 0);
+			glTranslatef(30, 0, 460.001);
+			Gedung3(1, 1, 1, 0, 1, 1, 1, 1, 0);
 		glPopMatrix();
 	glPopMatrix();
 	
@@ -113,7 +247,7 @@ void display()
 			glTranslatef(-20, 0, 6);
 			glRotatef(90, 0, 1, 0);
 			for (int i = 1; i <= nLstrk; i++) {
-				TiangListrik(0.2, 0.2, 0.2, 0.2, 0.2, 0.2);
+				TiangListrik(0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2);
 				if (i < nLstrk) {
 					KabelListrik(0.2, 0.2, 0.2, -30);
 				}
@@ -123,7 +257,7 @@ void display()
 		glPushMatrix();
 			glTranslatef(0, 0, 30);
 			for (int k = 1; k <= nGdg; k++) {
-				Gedung(0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2);
+//				Gedung(0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2);
 				glTranslatef(-90, 0, 0);
 			}
 		glPopMatrix();
@@ -140,8 +274,8 @@ void pressKey(int key, int x, int y)
 	switch (key) {
 		case GLUT_KEY_LEFT : deltaAngle = -0.015f; break;
 		case GLUT_KEY_RIGHT : deltaAngle = 0.015f; break;
-		case GLUT_KEY_UP : deltaMove = 3; break;
-		case GLUT_KEY_DOWN : deltaMove = -3; break;
+		case GLUT_KEY_UP : deltaMove = 5; break;
+		case GLUT_KEY_DOWN : deltaMove = -5; break;
 	}
 }
 
@@ -171,97 +305,89 @@ void pressKeyNormal(unsigned char key, int x, int y) {
 	switch (key) {
 		case 27:
 			exit(0);
-			
-		case 'p' :
-			KameraAtasFix();
+		case 'r':
+			posTrukX = 0;
+			posTrukZ = 0;
 			break;
+			
 		// gerakan truk
 		case 'w' :
-			if (cekHadapTruk == 0) {
+			if (posTrukX > -457) {
 				posTrukX -= 1;
 			} else {
-				posTrukX += 1;
+				posTrukX = -457;
 			}
 			
 			if (cekPalang == 0) {
-				if (CekTabrak(-15.5, -39.5, 7.5, -15.5) == 1)  {
-					posTrukX = -15.5;
-				}	
-			} else if (cekPalang == 1) {
-				if (CekTabrak(-15.5, -39.5, 5.5, -2.5) == 1)  {
-					posTrukX = -15.5;
+				if (CekTabrak(-65.5, -90.5, 1, -17) == 1)  {
+					posTrukX = -65.5;
 				}
-			}		
+			}
+			
+			if (CekTabrak(-146, -175, 1, -8) == 1)  {
+					posTrukX = -146;
+			} else if (CekTabrak(-186, -215, -8, -17) == 1)  {
+					posTrukX = -186;
+			} else if (CekTabrak(-226, -255, -7, -9) == 1)  {
+					posTrukX = -226;
+			}
 				
 			printf("posTrukX = %f\n", posTrukX);
 			break;
+			
 		case 's':
-			if (cekHadapTruk == 0) {
-				posTrukX += 0.5;
+			if (posTrukX < 0) {
+				posTrukX += 1;
 			} else {
-				posTrukX -= 0.5;
+				posTrukX = 0;
 			}
 			
 			if (cekPalang == 0) {
-				if (CekTabrak(-15.5, -39.5, 7.5, -15.5) == 1)  {
-					posTrukX = -39.5;
+				if (CekTabrak(-65.5, -90.5, 1, -17) == 1)  {
+					posTrukX = -90.5;
 				}	
-			} else if (cekPalang == 1) {
-				if (CekTabrak(-15.5, -39.5, 5.5, -2.5) == 1)  {
-					posTrukX = -39.5;
-				}
+			} 
+			
+			if (CekTabrak(-146, -175, 1, -8) == 1)  {
+					posTrukX = -175;
+			} else if (CekTabrak(-186, -215, -8, -17) == 1)  {
+					posTrukX = -215;
+			} else if (CekTabrak(-226, -255, -7, -9) == 1)  {
+					posTrukX = -255;
 			}
 			
 			printf("posTrukX = %f\n", posTrukX);
 			break;
+			
 		case 'a':
-			if (cekHadapTruk == 0) {
-				posTrukZ += 8;
-			} else {
-				posTrukZ -= 8;
+			if (posTrukZ < 0) {
+				posTrukZ += 8;	
 			}
 			
-			if (cekPalang == 0) {
-				if (CekTabrak(-15.5, -39.5, 7.5, -15.5) == 1)  {
-					posTrukZ = -15.5;
-				}	
-			} else if (cekPalang == 1) {
-				if (CekTabrak(-15.5, -39.5, 5.5, -2.5) == 1)  {
-					posTrukZ = -2.5;
-				}
+			if (CekTabrak(-146, -175, 1, -8) == 1)  {
+					posTrukZ = -8;
+			} else if (CekTabrak(-226, -255, -7, -9) == 1)  {
+					posTrukZ = -16;
 			}
 			
 			printf("posTrukZ = %f\n", posTrukZ);
 			break;
+			
 		case 'd':
-			if (cekHadapTruk == 0) {
-				posTrukZ -= 8;
-			} else {
-				posTrukZ += 8;
+			if (posTrukZ > -16) {
+				posTrukZ -= 8;	
 			}
 			
-			if (cekPalang == 0) {
-				if (CekTabrak(-15.5, -39.5, 7.5, -15.5) == 1)  {
-					posTrukZ = 7.5;
-				}	
-			} else if (cekPalang == 1) {
-				if (CekTabrak(-15.5, -39.5, 5.5, -2.5) == 1)  {
-					posTrukZ = 5.5;
-				}
+ 			if (CekTabrak(-186, -215, -8, -17) == 1)  {
+					posTrukZ = -8;
+			} else if (CekTabrak(-226, -255, -7, -9) == 1)  {
+					posTrukZ = 0;
 			}
 			
 			printf("posTrukZ = %f\n", posTrukZ);
 			break;
-		case 'l':
-			hadapTruk = 180;
-			cekHadapTruk = 1;
-			break;
-		case 'k':
-			hadapTruk = 0;
-			cekHadapTruk = 0;
-			break;
-			
-		// gerakan barang
+
+		// gerakan palang
 		case 'z':
 			if (rotPalang >= 0 && rotPalang < 90) {
 				rotPalang += 3;	
@@ -306,12 +432,3 @@ int CekTabrak(float depanB, float blkgB, float kiriB, float kananB) {
 		return 0;
 	}
 }
-
-
-
-
-//int CekTabrakMundur() {
-//	if ((posTrukX < -16 && posTrukX > -40.5) && (posTrukZ < 4.5 && posTrukZ > -4.5)) { // cek mundur
-//		return 1;
-//	}
-//}
